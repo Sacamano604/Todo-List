2edb258c958f514102e5d948e6cf7314365afa70
Sacamano604 <bentoussi@gmail.com>
2016-02-29 20:54:40 -0800

    Add complete task button/css changed
    Made the list a little more friendly. Now supports enter key press to
    add item. Added a complete task button which strikes through the li
    element. Also, edited the css a bunch.

--- js/todoList.js
+++ js/todoList.js
@@ -2,18 +2,39 @@
 function addItem() {
     var value = document.getElementById('userInput').value,
         taskListContainer = document.getElementById('task-list');
-    var html = '<article id="task-item" style="padding-bottom: 10px;">' + value + ' <button type="button" class="btn btn-danger">Remove</button>' +
-        '</article>';
+    var html = '<li id="task-item" style="padding-bottom: 10px;">' + value + ' <button type="button" class="btn btn-danger">Remove from List</button>' + ' <button type="button" class="btn btn-success">Mark Complete</button>' +
+        '</li>';
     taskListContainer.insertAdjacentHTML('beforeend', html);
+    document.getElementById('userInput').value = '';
 }
 //Function for deleting items
-var deleteItem = function(event){
+var deleteItem = function(event) {
     var isDeleteBtnClicked = event.target.className === 'btn btn-danger';
-    if(isDeleteBtnClicked){
+    if(isDeleteBtnClicked) {
         var taskElement = event.target.parentNode;
         document.getElementById('task-list').removeChild(taskElement);
     }
 }
-//event listeners for the specific functions to be called
-document.getElementById('task-list').addEventListener('click', deleteItem);
+//Function for marking items as complete. Will apply a classname which has a css line-through text-decoration applied.
+//No by design but when items are marked complete they go to the bottom of the list, must be with the classname change...
+var completeItem = function(event) {
+    var isCompleteBtnClicked = event.target.className === 'btn btn-success';
+    if (isCompleteBtnClicked) {
+        var taskElement = event.target.parentNode;
+        taskElement.className = 'complete';
+        document.getElementById('task-list').appendChild(taskElement);
+    }
+}
+//event listeners for the specific functions to be called with click or enter key
+document.getElementById('userInput').addEventListener('keypress', function(enter) {
+    if (enter.keyCode === 13) {
+        addItem();
+        enter.preventDefault();
+    }
+});
 document.getElementById('add-task').addEventListener('click', addItem);
+document.getElementById('task-list').addEventListener('click', deleteItem);
+document.getElementById('task-list').addEventListener('click', completeItem);
+
+
+
